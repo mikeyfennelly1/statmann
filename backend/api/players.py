067c2a3ec552193601a8776d92a77cb13d4d1426
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2023-12-12 19:02:40 +0000

    players list responding from nba_api

--- backend/api/players.py
+++ backend/api/players.py
@@ -1,9 +1,13 @@
 from flask import jsonify, request
 from . import bp
 
+from nba_api.stats.static import players
+
 @bp.route("/players", methods=["GET"])
 def get_players():
     # Get query parameters from the URL
     name = request.args.get('name')
+
+    players_list = players.get_players()
     # Your code to query the players dataset,
-    return jsonify({"message": name})
+    return jsonify(players_list)
