afd257b3a35a826d7b8e3e6c798967c7e73a1eb6
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-02-16 20:27:54 +0000

    scrap files added trying to figure out design

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,3 @@
 FROM openjdk:11
 COPY . /src/api/api
 WORKDIR /src/api/api
-RUN javac api.java
-CMD ["java", "api"]
--- src/api/api.java
+++ src/api/api.java
@@ -1,4 +1,4 @@
-public class api {
+public class Main {
     public static void main(String[] args) {
         System.out.println("this is the console logging method in java");
     }
